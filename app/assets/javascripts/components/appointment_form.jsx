cbd57d85f30ce9ff5c1c312b434112d62f761602
Jason Hanna <jason.hanna01@gmail.com>
2017-02-02 23:35:02 -0600

    add methods for form input

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,24 @@
 var AppointmentForm = React.createClass({
+  handleChange: function(e){
+    var name = e.target.name;
+    obj = {};
+    obj[name]=e.target.value;
+    this.props.onUserInput(obj);
+  },
+
   render: function() {
     return(
       <div>
         <h2>Make a new appointment</h2>
         <form>
-        <input name="title" placeholder="Appointment Name" />
-        <input name="appt_time" placeholder="Appointment Date and Time" />
+        <input name="title"
+          placeholder="Appointment Name"
+          value={this.props.input_title}
+          onChange={this.handleChange} />
+        <input name="appt_time"
+          placeholder="Appointment Date and Time"
+          value={this.props.input_appt_time}
+          onChange={this.handleChange} />
         <input type="submit" value="Make Appointment" />
       </form>
     </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,22 @@
 var Appointments = React.createClass({
+  getInitialState: function(){
+    return{
+      appointments: this.props.appointments,
+      title: 'Team meeting',
+      appt_time: 'Tommorrow at 9am'
+    }
+  },
+
+  handleUserInput: function(obj){
+    this.setState(obj)
+  },
   render: function(){
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={this.props.appointments} />
+        <AppointmentForm input_title={this.state.title}
+          input_appt_time={this.state.appt_time}
+          onUserInput={this.handleUserInput}/>
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
